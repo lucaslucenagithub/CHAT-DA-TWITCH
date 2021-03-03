c50d7a5b9cf193cec5078e7c391b58774391b41d
Lucas Lucena <ml.lucaslucena073@outlook.com>
2021-03-02 22:52:54 -0300

    initial commit with navigation and login page (not finished yet)

---
+++ app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+//Widgets
+import './widgets/login.dart';
+
+//Utils
+import './util/colors/createMaterialColor.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: createMaterialColor(new Color(0xFF6441a5)),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Navigator(
+          pages: [MaterialPage(child: LoginPage(title: 'Login'))],
+          onPopPage: (route, result) => route.didPop(result)),
+    );
+  }
+}
